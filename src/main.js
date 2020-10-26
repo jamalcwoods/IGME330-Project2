f4a6b92e88f25ef091b0cb095c2b0bcc154a333b
Jamal <jamalcwoods@gmail.com>
2020-10-26 16:12:19 -0400

    Added first set of controls for customization

--- src/main.js
+++ src/main.js
@@ -16,16 +16,29 @@ document.querySelector("audio").onplay = (e) => {
         audioCtx.resume();
     }
 };
+//Controls
 
-let beatSectionCount = 20;
+let beatSectionCount = 5;
 let beatSections = []
-for(var i = 0; i < beatSectionCount; i ++){
-    beatSections.push(0)
-}
-
+let color1 = "#000000",color2 = "#ffffff"
 let ctx = document.querySelector("canvas").getContext("2d");
 
 update();
+
+
+
+document.querySelector("#frequencySectionAmount").addEventListener("input",function(e){
+    beatSectionCount = parseInt(e.target.value)
+    document.querySelector("#frequencySphereInd").innerHTML = "Number of Frequency Spheres: " + e.target.value
+})
+
+document.querySelector("#color1").addEventListener("input",function(e){
+    color1 = e.target.value
+})
+
+document.querySelector("#color2").addEventListener("input",function(e){
+    color2 = e.target.value
+})
 
 
 function update(){
@@ -41,15 +54,30 @@ function update(){
 
 
     ctx.save()
-    ctx.fillStyle = "white"
-    ctx.fillRect(0,0,640,480)
+    var gradient1 = ctx.createLinearGradient(ctx.canvas.width/2,ctx.canvas.height/2,ctx.canvas.width/2,0);
+    gradient1.addColorStop(0, color1);
+    gradient1.addColorStop(val/255, color2);
+    gradient1.addColorStop(1- val/255, color1);
+    gradient1.addColorStop(1, color2);
+
+    var gradient2 = ctx.createLinearGradient(ctx.canvas.width/2,ctx.canvas.height/2, ctx.canvas.width/2,ctx.canvas.height);
+    gradient2.addColorStop(0, color1);
+    gradient2.addColorStop(val/255, color2);
+    gradient2.addColorStop(1- val/255, color1);
+    gradient2.addColorStop(1, color2);
+    
+    ctx.fillStyle = gradient1
+    ctx.fillRect(0,0,ctx.canvas.width,ctx.canvas.height/2)
+
+    ctx.fillStyle = gradient2
+    ctx.fillRect(0,ctx.canvas.height/2,ctx.canvas.width,ctx.canvas.height)
 
     ctx.beginPath()
     for(var i = 0; i < beatSectionCount;i++){
         var gapSize = (ctx.canvas.width/(beatSectionCount + 1))
         ctx.arc(ctx.canvas.width - (gapSize * (i+1)),ctx.canvas.height/2,(gapSize * (beatSections[i]/255)), 0, 2 * Math.PI);
     }
-    ctx.fillStyle = "red"
+    ctx.fillStyle = color2
     ctx.fill();
     ctx.restore();
 }
