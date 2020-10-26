38de536513153c34b90546e85561fd62388b0e92
Jamal <jamalcwoods@gmail.com>
2020-10-26 16:42:44 -0400

    added frequency lines

--- src/main.js
+++ src/main.js
@@ -72,11 +72,25 @@ function update(){
     ctx.fillStyle = gradient2
     ctx.fillRect(0,ctx.canvas.height/2,ctx.canvas.width,ctx.canvas.height)
 
+    for(var i = 0;i <  data.length; i++){
+        var y = ctx.canvas.height - ((ctx.canvas.height/data.length) * (i+1))
+        var x1 = (ctx.canvas.width/2) - ((ctx.canvas.width/4) *  data[i]/255)
+        var x2 = (ctx.canvas.width/2) + ((ctx.canvas.width/4) *  data[i]/255)
+        ctx.beginPath();
+        ctx.moveTo(x1,y)
+        ctx.lineTo(x2,y)
+        ctx.stroke()
+    }
+
     ctx.beginPath()
     for(var i = 0; i < beatSectionCount;i++){
         var gapSize = (ctx.canvas.width/(beatSectionCount + 1))
         ctx.arc(ctx.canvas.width - (gapSize * (i+1)),ctx.canvas.height/2,(gapSize * (beatSections[i]/255)), 0, 2 * Math.PI);
     }
+
+    
+    
+    
     ctx.fillStyle = color2
     ctx.fill();
     ctx.restore();
